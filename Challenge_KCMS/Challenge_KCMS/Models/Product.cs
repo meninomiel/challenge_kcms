292d1655e0a7a0ec2ec3169bd5244b95ff23dd8e
Edmiel Leandro Bandeira da Silva <meninomiel@hotmail.com>
2020-07-12 21:35:51 -0300

    Create the product class

---
+++ Challenge_KCMS/Challenge_KCMS/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenge_KCMS.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public string Image { get; set; }
+
+        //category
+    }
+}
