dba59f31f258dcc21bc078c47a3afdd99397ea9c
Edmiel Leandro Bandeira da Silva <meninomiel@hotmail.com>
2020-07-20 23:28:36 -0300

    Implement category repository

---
+++ Challenge_KCMS/Challenge_KCMS/Services/CategoryRepository.cs
@@ -0,0 +1,37 @@
+﻿using Challenge_KCMS.Data;
+using Challenge_KCMS.Interfaces.Repositories;
+using Challenge_KCMS.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Challenge_KCMS.Services
+{
+    class CategoryRepository : ICategoryRepository
+    {
+        DatabaseHelper _databaseHelper;
+        public CategoryRepository()
+        {
+            _databaseHelper = new DatabaseHelper();
+        }
+
+        public void DeleteAllCategories()
+        {
+            _databaseHelper.DeleteAllCategories();
+        }
+
+        public void DeleteCategory(int categoryId)
+        {
+            _databaseHelper.DeleteCategory(categoryId);
+        }
+
+        public List<Category> GetCategories()
+        {
+            return _databaseHelper.GetCategories();
+        }
+
+        public void InsertCategory(Category category)
+        {
+            _databaseHelper.InsertCategory(category);
+        }
+    }
+}
